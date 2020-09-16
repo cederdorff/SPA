73a643799f859186508398fcf40bb8fadafd089a
Sona <sona.juhasova@gmail.com>
2020-09-16 20:04:09 +0200

    js fixed

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function goMap() {
 
 //toggle button
 // source: https: //www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-
+/*
 function toggleNotifications() {
     var x = document.getElementById("toggleBtn1");
     var y = document.getElementById("toggleBG1");
@@ -36,7 +36,7 @@ function toggleNotifications() {
     }
 }
 
-function toggleNotifications() {
+function toggleFav() {
     var x = document.getElementById("toggleBtn2");
     var y = document.getElementById("toggleBG1");
     if (x.style.margin === "5px auto 5px 5px") {
@@ -47,24 +47,24 @@ function toggleNotifications() {
         y.style.background = "#ff5959";
     }
 
+*/
+// Onboarding mechanic
 
-    // Onboarding mechanic
-
-    const onBoardingButtons = document.querySelectorAll("#onboarding > div > button.next");
-    const onBoardingSkip = document.querySelectorAll("#onboarding .top_part .skip");
-    const onBoardingContainers = document.querySelectorAll("#onboarding > div");
+const onBoardingButtons = document.querySelectorAll("#onboarding > div > button.next");
+const onBoardingSkip = document.querySelectorAll("#onboarding .top_part .skip");
+const onBoardingContainers = document.querySelectorAll("#onboarding > div");
 
 
-    for (let i = 0; i < 4; i++) {
-        onBoardingButtons[i].addEventListener('click', () => {
+for (let i = 0; i < 4; i++) {
+    onBoardingButtons[i].addEventListener('click', () => {
+        onBoardingContainers[i].style.display = "none";
+    })
+}
+for (let j = 0; j < 4; j++) {
+    onBoardingSkip[j].addEventListener('click', () => {
+        for (let i = 0; i < 4; i++) {
             onBoardingContainers[i].style.display = "none";
-        })
-    }
-    for (let j = 0; j < 4; j++) {
-        onBoardingSkip[j].addEventListener('click', () => {
-            for (let i = 0; i < 4; i++) {
-                onBoardingContainers[i].style.display = "none";
-            }
-        })
-    }
-    // Single page application content //
+        }
+    })
+
+}
