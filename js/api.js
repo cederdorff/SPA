eafdc7641eb9a85f577c247a09932851f38d1cec
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2020-09-20 15:11:33 +0200

    hgkldfgf

--- js/api.js
+++ js/api.js
@@ -29,26 +29,16 @@
 
  //BestTimes API code
  //  var settings = {
- //       "url": "https://besttime.app/api/v1/keys/pri_0bc6ad2618fe4bb0b9092c84f41b3010",
+ //       "url": "https://besttime.app/api/v1/venues",
+ //       "data": {
+ //            'api_key_private': 'pri_0bc6ad2618fe4bb0b9092c84f41b3010'
+ //       },
  //       "method": "GET"
  //  };
 
  //  $.ajax(settings).done(function (response) {
  //       console.log(response);
  //  });
-
-
- var settings = {
-      "url": "https://besttime.app/api/v1/venues",
-      "data": {
-           'api_key_private': 'pri_0bc6ad2618fe4bb0b9092c84f41b3010'
-      },
-      "method": "GET"
- };
-
- $.ajax(settings).done(function (response) {
-      console.log(response);
- });
 
  var settings = {
       "url": "https://besttime.app/api/v1/forecasts?api_key_private=pri_0bc6ad2618fe4bb0b9092c84f41b3010&venue_name=ARoS Aarhus Art Museum&venue_address=Aros Allé 2 8000 Aarhus Denmark ",
@@ -64,19 +54,23 @@
       console.log(response);
       let data = response;
       let week = data.analysis;
-      for (let i = 0; i <= 6; i++) {
-           for (let j = 2; j <= 14; j = j + 2) {
-                //getting crowdedness of this place from 8 to 20 every 2 hours j = 2 is truly 8am and j = 14 is 8pm
-                let busyness = week[i].hour_analysis[j].intensity_nr;
-                busyness += 3;
-                console.log(busyness)
-                const charts = document.querySelectorAll("#home .graph-wrapper .graph-column")
-                for (let k = 0; k <= 6; k++) {
-                     charts[k].style.height = busyness[k] + "px"
-                     //trying to set a height of each chart using busyness
-                }
-           }
-      }
+      let busyness = [];
+      //  for (let i = 0; i <= 6; i++) {
+      //       for (let j = 2; j <= 14; j = j + 2) {
+      //            //getting crowdedness of this place from 8 to 20 every 2 hours j = 2 is truly 8am and j = 14 is 8pm
+      //            busyness.push(week[i].hour_analysis[j].intensity_nr);
+      //            //  week[i].hour_analysis[j].intensity_nr;
+      //            console.log(busyness)
+      //            const charts = document.querySelectorAll("#home .graph-wrapper .graph-column");
+      //            for (let k = 0; k <= 6; k++) {
+      //                 charts[k].style.height = busyness[k] + "px"
+      //                 //trying to set a height of each chart using busyness
+      //            }
+      //       }
+      //  }
+      var today = new Date();
+      var Day = today.getDay();
+      for (let i = 2; i <= 14; i + 2)[
+           week[Day].hour_analysis[i].intensity_nr
+      ]
  });
-
- 
